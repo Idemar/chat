4b6718bf22d5ba8a058ebf552893d6958901b548
Morten Idemar Andersen Tenmann <mortenidemar@gmail.com>
2025-07-23 21:25:55 +0200

    Profile pricture from oauth2

--- client.go
+++ client.go
@@ -28,9 +28,9 @@ func (c *client) read() {
 		}
 		msg.When = time.Now()
 		msg.Name = c.userData["name"].(string)
-		// if avatarURL, ok := c.userData["avatar_url"]; ok {
-		// 	msg.avatarURL = avatarURL.(string)
-		//}
+		if avatarURL, ok := c.userData["avatar_url"]; ok {
+			msg.AvatarURL = avatarURL.(string)
+		}
 		c.room.forward <- msg
 	}
 }
--- message.go
+++ message.go
@@ -4,7 +4,8 @@ import "time"
 
 // message representerer en enkelt melding
 type message struct {
-	Name    string
-	Message string
-	When    time.Time
+	Name      string
+	Message   string
+	When      time.Time
+	AvatarURL string
 }
